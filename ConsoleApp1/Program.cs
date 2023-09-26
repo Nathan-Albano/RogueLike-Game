92d504b5eba42adaac63c9897a4ddab54ff7501a
Nathan Albano <natealbano123@gmail.com>
2023-09-26 16:41:23 -0700

    Added comments

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,21 +9,26 @@ namespace RogueSharpV3Tutorial
         private static readonly int _screenHeight = 70 ;
         private static RLRootConsole _rootConsole;
 
+        //Map
         private static readonly int _mapWidth = 80;
         private static readonly int _mapHeight = 48;
         private static RLConsole _mapConsole;
 
+        //Message
         private static readonly int _messageWidth = 80;
         private static readonly int _messageHeight = 11;
         private static RLConsole _messageConsole;
 
+        //Stats
         private static readonly int _statWidth = 20;
         private static readonly int _statHeight = 70;
         private static RLConsole _statConsole;
 
+        //Inventory
         private static readonly int _inventoryWidth = 80;
         private static readonly int _inventoryHeight = 11;
         private static RLConsole _inventoryConsole;
+
         public static void Main()
         {
             string fontFileName = "terminal8x8.png";
@@ -34,8 +39,10 @@ namespace RogueSharpV3Tutorial
             _messageConsole = new RLConsole(_messageWidth, _messageHeight);
             _statConsole = new RLConsole(_statWidth, _statHeight);
             _inventoryConsole = new RLConsole(_inventoryWidth, _inventoryHeight);
+
             _rootConsole.Update += OnRootConsoleUpdate;
             _rootConsole.Render += OnRootConsoleRender;
+
             _rootConsole.Run();
         }
 
@@ -52,11 +59,11 @@ namespace RogueSharpV3Tutorial
 
             _inventoryConsole.SetBackColor(0, 0, _inventoryWidth, _inventoryHeight, RLColor.Cyan);
             _inventoryConsole.Print(1, 1, "INVENTORY", RLColor.White);
-
         }
 
         private static void OnRootConsoleRender(object sender, UpdateEventArgs e) 
         {
+            //Blit
             RLConsole.Blit(_mapConsole, 0, 0, _mapWidth, _mapHeight, _rootConsole, 0, _inventoryHeight);
             RLConsole.Blit(_statConsole, 0, 0, _statWidth, _statHeight, _rootConsole, _mapWidth, 0);
             RLConsole.Blit(_messageConsole, 0, 0, _messageWidth, _messageHeight,  _rootConsole, 0, _screenHeight - _messageHeight);
