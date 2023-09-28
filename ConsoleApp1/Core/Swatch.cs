3a891009a4475b6c1d310409d9d5f3095872ddc9
gurjot singh <har03gur@gmail.com>
2023-09-27 17:55:10 -0700

    Added colors and some map parts

--- ConsoleApp1/Core/Swatch.cs
+++ ConsoleApp1/Core/Swatch.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RLNET;
 
 namespace ConsoleApp1.Core
 {
     internal class Swatch
     {
+        public static RLColor Gold1 = new RLColor(255, 226, 185);
+        public static RLColor Gold2 = new RLColor(254, 208, 142);
+        public static RLColor Gold3 = new RLColor(216, 163, 89);
+        public static RLColor Gold4 = new RLColor(187, 130, 50);
+        public static RLColor Gold5 = new RLColor(146, 94, 21);
+        public static RLColor Blue1 = new RLColor(156, 167, 207);
+        public static RLColor Blue2 = new RLColor(105, 120, 172);
+        public static RLColor Blue3 = new RLColor(70, 87, 147);
+        public static RLColor Blue4 = new RLColor(45, 63, 127);
+        public static RLColor Blue5 = new RLColor(24, 41, 99);
+        public static RLColor Cyan1 = new RLColor(141, 195, 195);
+        public static RLColor Cyan2 = new RLColor(85, 152, 152);
+        public static RLColor Cyan3 = new RLColor(53, 130, 130);
+        public static RLColor Cyan4 = new RLColor(30, 112, 112);
+        public static RLColor Cyan5 = new RLColor(13, 88, 88);
     }
 }
